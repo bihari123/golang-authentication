6aa95e29dc2f791abbf8b48017bbdbbe74485d18
bihari123 <thakur.cs.tarun@gmail.com>
2022-05-10 10:59:35 +0530

    json encode example

--- json-encoding/json-encoding-decoding.go
+++ json-encoding/json-encoding-decoding.go
@@ -1,8 +1,7 @@
-package main
+package jsonencoding
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -11,40 +10,49 @@ type person struct {
 	First string
 }
 
-func main() {
+func JsonEncodingDecoding() {
+	// p1 := person{
+	// 	First: "jenny",
+	// }
+	// p2 := person{
+	// 	First: "james",
+	// }
+	//
+	// xp := []person{p1, p2}
+	//
+	// bs, err := json.Marshal(xp)
+	//
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+	// xp2 := []person{}
+	// err = json.Unmarshal(bs, &xp2)
+	//
+	// if err != nil {
+	// 	fmt.Println(" back to golang structure", xp2)
+	// }
+	//
+	http.HandleFunc("/encode", foo)
+	http.HandleFunc("/decode", bar)
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		log.Panic("server did not set up")
+	}
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+
 	p1 := person{
 		First: "jenny",
 	}
-	p2 := person{
-		First: "james",
-	}
 
-	xp := []person{p1, p2}
+	err:=json.NewEncoder(w).Encode(p1)
 
-	bs,err:=json.Marshal(xp)
-
-	if err!=nil{
-		log.Panic(err)
-	}
-	xp2:=[]person{}
-	err= json.Unmarshal(bs,&xp2)
-
-	if err!=nil{
-		fmt.Println(" back to golang structure",xp2) 
-	}
-
-	http.HandleFunc("/encode",foo)
-	http.HandleFunc("/decode",bar)
-	err = http.ListenAndServe(":8080",nil)
-
-	if err != nil{
-log.Panic("server did not set up")
+	if err !=nil{
+		log.Println("Encoded bad data",err)
 	}
 }
-
-func foo(w http.ResponseWriter,r *http.Request){
-
-}
-func bar (w http.ResponseWriter,r *http.Request){
+func bar(w http.ResponseWriter, r *http.Request) {
 
 }
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	jsonencoding "github.com/bihari123/golang-authentication/json-encoding"
+)
+
+func main(){
+  jsonencoding.JsonEncodingDecoding()
+}
