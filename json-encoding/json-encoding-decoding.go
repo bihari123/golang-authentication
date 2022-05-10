c77b60cbbc673729c9c6653c700e60af144fc2b5
bihari123 <thakur.cs.tarun@gmail.com>
2022-05-10 10:39:29 +0530

    server set up

--- json-encoding/json-encoding-decoding.go
+++ json-encoding/json-encoding-decoding.go
@@ -1,6 +1,11 @@
 package main
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 type person struct {
 	First string
@@ -21,4 +26,25 @@ func main() {
 	if err!=nil{
 		log.Panic(err)
 	}
+	xp2:=[]person{}
+	err= json.Unmarshal(bs,&xp2)
+
+	if err!=nil{
+		fmt.Println(" back to golang structure",xp2) 
+	}
+
+	http.HandleFunc("/encode",foo)
+	http.HandleFunc("/decode",bar)
+	err = http.ListenAndServe(":8080",nil)
+
+	if err != nil{
+log.Panic("server did not set up")
+	}
+}
+
+func foo(w http.ResponseWriter,r *http.Request){
+
+}
+func bar (w http.ResponseWriter,r *http.Request){
+
 }
