3a6a555a07f04a5f6c4e1ffa433afe6027492808
bihari123 <thakur.cs.tarun@gmail.com>
2022-05-11 10:11:11 +0530

    jwt create claims

--- go.mod
+++ go.mod
@@ -3,5 +3,3 @@ module github.com/bihari123/golang-authentication
 go 1.18
 
 require golang.org/x/crypto v0.0.0-20220507011949-2cf3adece122
-
-require github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
--- http-auth/jwt-authentication.go
+++ http-auth/jwt-authentication.go
@@ -1,2 +1,23 @@
 package httpauth
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type UserClaims struct{
+  jwt.StandardClaims
+  SessionID int64 
+}
+
+func (u *UserClaims)Valid()error{
+  if !u.VerifyExpiresAt(time.Now().Unix(),true){
+    return fmt.Errorf("Token has expired")
+  }
+  if u.SessionID == 0{
+    return fmt.Errorf("invalid session ID")
+  }
+  return nil 
+}
